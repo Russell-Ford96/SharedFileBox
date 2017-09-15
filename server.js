805d18d140d2523ec8d74ded2a5a3116a0ea3275
Russell <russellford96@gmail.com>
2017-09-15 15:26:42 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,29 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Tell node what routes to use
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+// Get port from env or default to 5000
+const port = process.env.PORT || '5000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
