fda9eef847a8b6a171740cbbaf8e55cc60b160af
Nikitha G <nikithagmn@gmail.com>
2017-09-18 21:07:38 -0400

    file upload component

--- src/app/file-upload.component.ts
+++ src/app/file-upload.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
     selector:'app-file-upload',
@@ -6,5 +7,25 @@ import {Component} from "@angular/core";
 })
 export class FileUploadComponent{
 
-
+  //still working on this
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      let formData: FormData = new FormData();
+      formData.append('uploadFile', file, file.name);
+      let headers = new Headers();
+      /** No need to include Content-Type in Angular 4 */
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+      let options = new RequestOptions({headers: headers});
+      this.http.post(`${this.apiEndPoint}`, formData, options)
+        .map(res => res.json())
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => console.log('success'),
+          error => console.log(error)
+        );
+    }
+  }
 }
