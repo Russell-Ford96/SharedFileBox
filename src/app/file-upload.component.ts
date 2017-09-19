6a17f58dcfa646ac2a8c61f813065b29808fb923
Nikitha G <nikithagmn@gmail.com>
2017-09-18 20:23:17 -0400

    file upload component

---
+++ src/app/file-upload.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'app-file-upload',
+    templateUrl: './file-upload.component.html'
+})
+export class FileUploadComponent{
+
+
+}
