b515207b2dd9bcbfea2ea25c37d262b3ab9368c8
Russell <russellford96@gmail.com>
2017-09-21 11:10:51 -0400

    added dummy data to request component

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -6,6 +6,18 @@ import { Component }                            from '@angular/core';
 })
 export class RequestComponent {
   showRequestForm = false;
+    dummyRequest = {
+        _id: "1234567890fakeID",
+        email: "test@example.com",
+        phone: "+19545555555",
+        docArray: [
+            { name: "ourNameForTheDoc",
+                attachment: "nameOfFileOnServer.ext"
+            }
+        ],
+        thanks: "thank you message"
+    };
+    dummyTableData = [this.dummyRequest, this.dummyRequest];
 
   toggleShowRequest(): void {
     this.showRequestForm = !this.showRequestForm;
