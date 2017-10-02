b1064959a7e8ee7e68c3d79372aca393bcded6f0
Nikitha G <nikithagmn@gmail.com>
2017-10-02 16:40:49 -0400

    navigation

---
+++ src/app/leftsidenav/leftsidenav.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'left-sidebar',
+  templateUrl:'./leftsidenav.component.html',
+
+})
+
+export class LeftsidenavComponent{
+
+}
+
