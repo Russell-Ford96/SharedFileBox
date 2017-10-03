74c75fcd36bb1bff9cab7f056705aea7f8ff1a65
Benjamin ALvarez <benjaminao@gmail.com>
2017-10-03 17:09:45 -0400

    adding sound to sent messages

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -15,8 +15,7 @@ import { AppService } from '../app.service';
 })
 
 export class FileUploadComponent {
-  // public uploader: FileUploader = new FileUploader({ url: 'http://localhost:5000/api/upload', itemAlias: "single", autoUpload: true
-  public uploader: FileUploader = new FileUploader({ url: 'http://10.34.24.238:5000/api/upload', itemAlias: "single", autoUpload: true
+  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:5000/api/upload', itemAlias: "single", autoUpload: true
   });
 
   id: any;
